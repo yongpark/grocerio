eafc65d4f8dd9317b1486c04286ecc94b5b1e0fc
yongpark <yongpark.sd@gmail.com>
2017-01-10 14:55:01 -0800

    testing render #2

--- frontend/grocero.jsx
+++ frontend/grocero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import configureStore from './store/store.js';
+
 import Root from './components/root';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
