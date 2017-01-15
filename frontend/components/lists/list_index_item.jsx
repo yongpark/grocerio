8411b291e64bffec311fb27052355256959ce5cd
yongpark <yongpark.sd@gmail.com>
2017-01-14 23:33:16 -0800

    fixed text decoration for cards

--- frontend/components/lists/list_index_item.jsx
+++ frontend/components/lists/list_index_item.jsx
@@ -1,27 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-const style = {
-  height: 80,
-  width: 100,
-  margin: 20,
-  textAlign: 'center',
-  textDecoration: 'none',
-  display: 'block',
-};
-
-
 const ListIndexItem = ({list}) => (
   <li className='list-box'>
     <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-      <Link to={`/lists/${list.id}`}>
-        <Paper style={style} zDepth={2}>
-          {list.title}
-        </Paper>
+      <Link to={`/lists/${list.id}`}  style={{ textDecoration: 'none' }}>
+        <Card className="list-box">
+          <CardTitle title={list.title}/>
+        </Card>
       </Link>
     </MuiThemeProvider>
   </li>
