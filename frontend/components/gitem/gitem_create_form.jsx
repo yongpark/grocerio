7a07c4c9ab78ca3a9dab34d790d39d4560d4445f
yongpark <yongpark.sd@gmail.com>
2017-05-17 19:30:44 -0700

    fixes date picker default value after submit

--- frontend/components/gitem/gitem_create_form.jsx
+++ frontend/components/gitem/gitem_create_form.jsx
@@ -31,6 +31,8 @@ class GItemCreateForm extends React.Component {
     this.handleDatepickerOnClose = this.handleDatepickerOnClose.bind(this);
     this.reset = this.reset.bind(this);
     this.initialState = this.state.gitem;
+    this.formatDateString = this.formatDateString.bind(this);
+    this.clearDate = this.clearDate.bind(this);
   }
 
    componentDidMount() {
@@ -106,7 +108,7 @@ class GItemCreateForm extends React.Component {
    handleSubmit(e){
      e.preventDefault();
      if(this.state.gitem){
-       this.props.createGItem(this.state.gitem).then(this.setState({gitem: this.initialState})).then(this.reset).then(this.hide);
+       this.props.createGItem(this.state.gitem).then(this.setState({gitem: this.initialState})).then(this.reset).then(this.clearDate).then(this.hide);
      }
    }
 
@@ -116,6 +118,25 @@ class GItemCreateForm extends React.Component {
 
    handleDatepickerOnClose(e){
      this.datepickerOpen = false;
+   }
+
+   formatDateString(date){
+     let parsedDate = Date.parse(date);
+     let newDate = new Date(parsedDate);
+     newDate.setHours(0, 0, 0, 0);
+     console.log(newDate);
+   }
+
+   clearDate (event) {
+       event.preventDefault();
+
+       // We manually reach into the composed component and set it's date to null.
+       let newDate = null;
+       this.refs.datePicker.setState({
+           date: newDate
+       }, () => {
+           this.refs.datePicker.props.onChange(null, newDate);
+       });
    }
 
   //  this.renderCreateForm = this.state.gitem.column_title === 'To Buy' || this.state.gitem.column_title === 'Bought'
@@ -143,10 +164,11 @@ class GItemCreateForm extends React.Component {
                  />
                <DatePicker className="grocery-item-datepicker" onTouchTap={this.handleDatepickerOnShow}
                onChange={this.updateDate}
-               value={this.state.gitem.expire_date}
+               ref="datePicker"
+               value={this.state.gitem.expire_date ? new Date(this.state.gitem.expire_date) : null}
                onDismiss={this.handleDatepickerOnClose}
                container="inline" mode="landscape"/>
-             <RaisedButton type="submit" secondary={true} label="Save"/>
+             <RaisedButton ref="button" type="submit" secondary={true} label="Save"/>
            </Card>
            </MuiThemeProvider>
          </form>
