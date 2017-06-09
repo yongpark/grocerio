93856aed742e68c49468bd817e3b8b937e86411b
yongpark <yongpark.sd@gmail.com>
2017-06-09 10:40:59 -0700

    style updates list create and recipe

--- frontend/components/lists/list_create_form.jsx
+++ frontend/components/lists/list_create_form.jsx
@@ -8,7 +8,15 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
 import {Card, CardTitle} from 'material-ui/Card';
 import merge from 'lodash/merge';
-import {grey50} from 'material-ui/styles/colors';
+import {blueGrey300, blueGrey600, blueGrey700} from 'material-ui/styles/colors';
+import {blueGrey900} from 'material-ui/styles/colors';
+import {grey50, blueGrey100, blueGrey400} from 'material-ui/styles/colors';
+import {lightBlue300} from 'material-ui/styles/colors';
+
+const labelstyle={
+  fontWeight: 300,
+  color: grey50
+};
 
 
 class ListCreateForm extends React.Component{
@@ -80,18 +88,20 @@ class ListCreateForm extends React.Component{
         <div className="list-create-container" ref='create'>
           <form onSubmit={this.handleSubmit}>
             <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-              <Card className='list-create'>
-                <CardTitle className="card-subtitle" title="New Grocery List" subtitle="Grocery List Name" titleStyle={{color: grey50, fontSize: 18}} subtitleStyle={{color: grey50}}/>
+              <Card className='list-create' style={{backgroundColor:blueGrey100}}>
+                <CardTitle className="card-subtitle" title="New Grocery List" subtitle="Grocery List Name" ttitleStyle={{color: blueGrey900, fontSize: 18}} subtitleStyle={{color: blueGrey900}}/>
                 <TextField
                   id="titleinput"
                   type="text"
                   floatingLabelText="Weekly Grocery List"
                   onChange={this.update('title')}
-                  inputStyle={{paddingLeft: 5, paddingRight: 5, color: grey50, }}
-                  floatingLabelStyle={{color: grey50}}
-                  floatingLabelFocusStyle={{color: grey50}}
+                  inputStyle={{paddingLeft: 5, paddingRight: 5, color: blueGrey900, width:'100%'}}
+                  floatingLabelStyle={{color: blueGrey600,  left: '5px'}}
+                  floatingLabelFocusStyle={{color: blueGrey600}}
+                  hintStyle={{color: blueGrey900}}
+                  underlineFocusStyle={{width: '200px', borderColor:blueGrey700}}
                 />
-              <RaisedButton type="submit" secondary={true} label="Create"/>
+              <RaisedButton type="submit" label="Create" backgroundColor="#263238" labelStyle={{fontWeight: 300, color: grey50}}/>
             </Card>
             </MuiThemeProvider>
           </form>
