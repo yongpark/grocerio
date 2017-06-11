2f30816c960ec209c5d3c4c73c319d3bc84da8ba
yongpark <yongpark.sd@gmail.com>
2017-06-10 23:30:30 -0700

    updates to production scripts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.serve_static_files = true
+  config.public_file_server.enabled = true
   config.log_level = :debug
   # Code is not reloaded between requests.
   config.cache_classes = true
