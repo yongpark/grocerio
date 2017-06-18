bafbe33814b1c87ce404910e9e12205a9b991b94
yongpark <yongpark.sd@gmail.com>
2017-06-17 23:48:04 -0700

    seeds list #2 and adds list #4

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,21 +15,26 @@ list1column1 = list1.columns.create(list_id: list1.id, title: "To Buy", column_t
 list1column2 = list1.columns.create(list_id: list1.id, title: "Bought", column_type: "bought")
 list1column3 = list1.columns.create(list_id: list1.id, title: "Expired", column_type: "expired")
 list1column4 = list1.columns.create(list_id: list1.id, title: "Used", column_type: "used")
-list2 = guest.owned_lists.create({user_id: 1, title: "4th of July Grocery list"})
+list2 = guest.owned_lists.create({user_id: 1, title: "Lunch Meal Prep Grocery list"})
 list2column1 = list2.columns.create(list_id: list2.id, title: "To Buy", column_type: "to_buy")
 list2column2 = list2.columns.create(list_id: list2.id, title: "Bought", column_type: "bought")
 list2column3 = list2.columns.create(list_id: list2.id, title: "Expired", column_type: "expired")
 list2column4 = list2.columns.create(list_id: list2.id, title: "Used", column_type: "used")
-list3 = guest.owned_lists.create({user_id: 1, title: "Healthy Grocery list"})
+list3 = guest.owned_lists.create({user_id: 1, title: "Dinner Grocery list"})
 list3column1 = list3.columns.create(list_id: list3.id, title: "To Buy", column_type: "to_buy")
 list3column2 = list3.columns.create(list_id: list3.id, title: "Bought", column_type: "bought")
 list3column3 = list3.columns.create(list_id: list3.id, title: "Expired", column_type: "expired")
 list3column4 = list3.columns.create(list_id: list3.id, title: "Used", column_type: "used")
+list4 = guest.owned_lists.create({user_id: 1, title: "Snack List"})
+list4column1 = list4.columns.create(list_id: list4.id, title: "To Buy", column_type: "to_buy")
+list4column2 = list4.columns.create(list_id: list4.id, title: "Bought", column_type: "bought")
+list4column3 = list4.columns.create(list_id: list4.id, title: "Expired", column_type: "expired")
+list4column4 = list4.columns.create(list_id: list4.id, title: "Used", column_type: "used")
+
 
 list1column1.gitems.create(title: 'cereal', ord: 1)
 list1column1.gitems.create(title: 'oatmeal', ord: 2)
 list1column1.gitems.create(title: 'grapefruit', ord: 3)
-
 
 list1column2.gitems.create(title: 'flour', ord: 1, expire_date: DateTime.new(2017,month,day))
 list1column2.gitems.create(title: 'oranges', ord: 2, expire_date: DateTime.new(2017,month,day))
@@ -42,3 +47,23 @@ list1column3.gitems.create(title: 'avocados', ord: 2)
 list1column4.gitems.create(title: 'bananas', ord: 1)
 list1column4.gitems.create(title: 'yogurt', ord: 2)
 list1column4.gitems.create(title: 'bacon', ord: 3)
+
+list2column1.gitems.create('quinoa', ord: 1)
+list2column1.gitems.create('celery', ord: 2)
+list2column1.gitems.create('carrots', ord: 3)
+list2column1.gitems.create('peanut butter', ord: 4)
+
+list2column2.gitems.create(title: 'brown rice', ord: 1, expire_date: DateTime.new(2017,month,day))
+list2column2.gitems.create(title: 'chicken', ord: 1, expire_date: DateTime.new(2017,month,day))
+list2column2.gitems.create(title: 'cauliflower', ord: 1, expire_date: DateTime.new(2017,month,day))
+list2column2.gitems.create(title: 'spinach', ord: 1, expire_date: DateTime.new(2017,month,day))
+list2column2.gitems.create(title: 'brocolli', ord: 1, expire_date: DateTime.new(2017,month,day))
+
+list2column3.gitems.create('steak', ord: 1)
+list2column3.gitems.create('sweet potatoes', ord: 2)
+
+list2column4.gitems.create('green pepper', ord: 1)
+list2column4.gitems.create('yogurt', ord: 2)
+list2column4.gitems.create('onion', ord: 3)
+
+list3column1.gitems.create(title: 'risotto', ord: 1)
